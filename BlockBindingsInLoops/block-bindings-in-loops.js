8988455d9e053163ffc1f2bb56d7b533d5a3c8c1
simon-towler <simon.towler@icloud.com>
2019-03-02 20:09:08 +0000

    Replace var with let.

--- BlockBindingsInLoops/block-bindings-in-loops.js
+++ BlockBindingsInLoops/block-bindings-in-loops.js
@@ -1,13 +1,16 @@
+//run node with --harmony flag
+"use strict"//extended mode
+
 const items = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"];
 function process(item) {
 	//do nothing
 }
 
-for (var i=0; i<10; i++) {
+for (let i=0; i<10; i++) {
 	process(items[i]);
 
 	console.log(i); // 0-9
 }
 
-//i is still accessible here
-	console.log(i); // 10
+//i is not still accessible here
+	//console.log(i); // error i is not defined
