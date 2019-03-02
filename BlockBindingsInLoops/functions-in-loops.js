33f16dbcde6ab38b3ce0ab0026f6ebc75c35fbe6
simon-towler <simon.towler@icloud.com>
2019-03-02 23:01:25 +0000

    Use Immediately Inoked Function Expression to log 0-9.

--- BlockBindingsInLoops/functions-in-loops.js
+++ BlockBindingsInLoops/functions-in-loops.js
@@ -1,11 +1,13 @@
 var funcs = [];
 
 for (var i=0; i < 10; i++) {
-	funcs.push(function() {
-		console.log(i);
-	});
+	funcs.push((function(value) {
+		return function() {
+			console.log(value);
+		}
+	}(i)));
 }
 
 funcs.forEach(function(func) {
-	func(); // outputs the number "10" ten times
+	func(); // outputs 0, then 1, then 2, up to 9
 });
