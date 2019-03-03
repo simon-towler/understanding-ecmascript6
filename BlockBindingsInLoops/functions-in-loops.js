a65ddc3f8dd8346d27e88179d51012577ac9a728
simon-towler <simon.towler@icloud.com>
2019-03-03 02:53:57 +0000

    Replaced var with let to get it to log 0-9.

--- BlockBindingsInLoops/functions-in-loops.js
+++ BlockBindingsInLoops/functions-in-loops.js
@@ -1,13 +1,14 @@
+//run node with --harmony flag
+"use strict"//extended mode
+
 var funcs = [];
 
-for (var i=0; i < 10; i++) {
-	funcs.push((function(value) {
-		return function() {
-			console.log(value);
-		}
-	}(i)));
+for (let i=0; i < 10; i++) {
+	funcs.push(function() {
+			console.log(i);
+	});
 }
 
 funcs.forEach(function(func) {
-	func(); // outputs 0, then 1, then 2, up to 9
+	func(); // should output 0, then 1, then 2, up to 9 -- but doesn't on node v0.10.33
 });
