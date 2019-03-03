63b41f696cb1364314791259bd39b221bbc7f848
simon-towler <simon.towler@icloud.com>
2019-03-03 03:56:22 +0000

    for...in for...of exercise with let.

---
+++ BlockBindingsInLoops/for_in-and-for_of-loops.js
@@ -0,0 +1,17 @@
+var funcs = [],
+	object = {
+		a:true,
+		b:true,
+		c:true
+	};
+
+for (let key in object) {
+	funcs.push(function() {
+		console.log(key);
+	});
+
+}
+
+funcs.forEach(function(func) {
+	func(); //outputs "a", then "b", then "c"
+});
