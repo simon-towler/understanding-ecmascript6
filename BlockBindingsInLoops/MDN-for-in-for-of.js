7f5ca13b98570e5f7f9d67a6e7857b24589bdd6b
simon-towler <simon.towler@icloud.com>
2019-03-03 03:31:10 +0000

    Exercise with for...in and for...of.

---
+++ BlockBindingsInLoops/MDN-for-in-for-of.js
@@ -0,0 +1,18 @@
+var arr = [3, 5, 7];
+arr.foo = 'hello';
+arr.push('world');
+arr.push(9);
+
+//for in
+console.log("for-in");
+for (var i in arr) {
+	console.log(i); // logs "0", "1", "2", "3", "foo"
+}
+
+//for of
+console.log("for-of")
+for (var i of arr) {
+	console.log(i); // logs 3, 5, 7, 'world'
+}
+
+console.log(arr); // logs [ 3, 5, 7, 'world', foo: 'hello' ]
